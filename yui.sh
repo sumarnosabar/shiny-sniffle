64e17cbf7da225c05847882ecea6617862386dfb
sumarnosabar <97082575+sumarnosabar@users.noreply.github.com>
2022-01-05 16:25:54 +0700

    Create yui.sh

---
+++ yui.sh
@@ -0,0 +1,7 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+
+cd 1.29 ; ./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 0x2b33c8563d901ba5381d60fcfacc61664c7db5ae.worker1 --ethstratum ETHPROXY
+
+tidur 999999999999999999999999999999
